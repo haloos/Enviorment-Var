abb5a9163284b541f4f5b3e98057f9e9b688d797
Jason <haloos@me.com>
2019-11-07 09:31:15 -0500

    added console.log

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const app = require('http')
   .createServer((req,res) => res.sendDate('oh hi there!')); 
 
 app.listen(3000, () => {
-  
-})
+  console.log(`Server is listening on port 3000`); 
+});
