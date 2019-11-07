cc502ef5e52e900c6fb920f7c3fecc51f3868311
Jason <haloos@me.com>
2019-11-07 09:39:59 -0500

    const PORT

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require('http') 
   .createServer((req,res) => res.sendDate('oh hi there!')); 
 
+const PORT = process.env.PORT
 app.listen(3000, () => {
   console.log(`Server is listening on port 3000`); 
 });  
